e098905d0f15f00317a62dac99fd4c62980b645d
Máté Galavics <mategalavics@gmail.com>
2025-10-24 16:09:52 +0200

    HomePage layout

--- obj/Debug/net9.0-ios/iossimulator-x64/RJVTD2_MP_2025261.AssemblyInfo.cs
+++ obj/Debug/net9.0-ios/iossimulator-x64/RJVTD2_MP_2025261.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RJVTD2_MP_2025261")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f8438e6e334288443e605afea0361e687764245e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+429e09764dd50896f5e1c37de44db68e1bc6cdf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RJVTD2_MP_2025261")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RJVTD2_MP_2025261")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

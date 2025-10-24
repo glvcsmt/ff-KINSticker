429e09764dd50896f5e1c37de44db68e1bc6cdf0
Máté Galavics <mategalavics@gmail.com>
2025-10-24 15:41:31 +0200

    Home Page added

---
+++ Views/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJVTD2_MP_2025261.Views;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
